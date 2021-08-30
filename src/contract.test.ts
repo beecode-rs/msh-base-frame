1d978784a433778511bd09dcdbdc4fcc39e39b15
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2021-08-30 22:35:25 +0200

    chore: update project

---
+++ src/contract.test.ts
@@ -0,0 +1,4 @@
+import { contractorTestRunner } from '@beecode/msh-test-contractor/lib/contract/contractor-test-runner'
+
+contractorTestRunner.dir('./src')
+// contractorTestRunner.file('./src/jest-spy/jest-spy-service.contract.ts')
---
+++ src/use-case/action-use-case.ts
@@ -0,0 +1,13 @@
+import { gitUseCase } from 'src/use-case/git-use-case'
+import { logger } from 'src/util/logger'
+
+export const actionUseCase = {
+  clone: async (): Promise<void> => {
+    await gitUseCase.cleanAndGetNewCopyOfTemplateRepo()
+    await gitUseCase.extractAndRemoveZipFileAndPrepareTempFolder()
+    await gitUseCase.renderAllTemplateWithValuesFromConfig()
+    await gitUseCase
+      .copyFilesFromBaseIfTheyDontExist()
+      .catch((err) => logger.error('Some file already exist, you need to compare folder manually', { err }))
+  },
+}
