80ae9d779a9846c3f4c0f0b36a25e7c734d2f6f8
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2021-09-04 00:23:16 +0200

    test: adding contracts
    refactor code for easier testing

--- src/util/constant.contract.ts
+++ src/util/constant.contract.ts
@@ -1,15 +1,33 @@
 import * as constant from './constant'
 import { contractFactory } from '@beecode/msh-test-contractor/lib/contract/contractor-factory'
+import { SpecialFnName } from '@beecode/msh-test-contractor/lib/enum/special-fn-name'
+import { ContractMockRevertFns } from '@beecode/msh-test-contractor/lib/types'
 
-export default contractFactory(constant, 'constant', {
-  configFilePath: {
+export default contractFactory(
+  {
+    module: constant,
+    subjectName: 'constant',
     mock: {
-      jest: (jest: any): any => {
-        // TODO check the problem with return type
+      jest: (): ContractMockRevertFns => {
         const spyCwd: jest.SpyInstance = jest.spyOn(process, 'cwd')
         spyCwd.mockReturnValue('/home/dummy')
         return [(): void => spyCwd.mockRestore()]
       },
     },
   },
-})
+  {
+    [SpecialFnName.SELF]: {
+      terms: [
+        {
+          params: [],
+          result: {
+            configFilePath: '/home/dummy/.base-frame',
+            tempFolderPath: '/home/dummy/.base-frame-tmp',
+            templateZipName: 'template.zip',
+            templateZipPath: '/home/dummy/.base-frame-tmp/template.zip',
+          },
+        },
+      ],
+    },
+  }
+)
